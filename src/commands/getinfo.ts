3f1bf2a663a31a5070f0219c7a32df28af7fb5ba
gramanicu <gramanicu@gmail.com>
2020-09-15 13:31:09 +0300

    Finished the riot api

--- src/commands/getinfo.ts
+++ src/commands/getinfo.ts
@@ -1,7 +1,7 @@
 import * as Discord from "discord.js"
 import IBotCommand from '../iBotCommand'
-import { LolApi as TwistedAPI, Constants as LolConstants } from 'twisted'
-import * as lolApi from '../lolApi'
+import * as LolApi from "../lolApi";
+import axios from 'axios'
 
 class CmdTemplate implements IBotCommand {
     readonly cmdName = 'getinfo';
@@ -16,35 +16,64 @@ class CmdTemplate implements IBotCommand {
 
     public async execute(args: string[], msgObject: Discord.Message, client: Discord.Client): Promise<void> {
         msgObject.delete().catch(process.stderr.write);
-        let summData: lolApi.SummonerData;
 
-        await lolApi.summonerByName(args[0], args[1])
-            .then(data => {
-                summData = data;
+        const regServer = LolApi.checkRegion(args[1]);
+
+        if (regServer) {
+            const baseServer = `${regServer}${LolApi.apiRoute}`;
+            const route = `https://${baseServer}${LolApi.summonerDataRoute}${args[0]}?api_key=${process.env.RIOT_TOKEN}`
+            axios.get(route).then(res => {
+                const encrySumm = res.data['id'];
+                const summLevel = res.data['summonerLevel'];
+
+                let embed = new Discord.MessageEmbed()
+                .setColor('#0099ff')
+                .setTitle(`Here is some info about ${res.data['name']} (${args[1].toUpperCase()})`)
+                .setURL('https://github.com/gramanicu/ShroomBot#readme')
+                .setThumbnail('https://cdn.discordapp.com/app-icons/755011946654335034/5f1aed402fe3b8fb61df8e397510e858.png');
+
+                const route2 = `https://${baseServer}${LolApi.champMasteryRoute}${encrySumm}?api_key=${process.env.RIOT_TOKEN}`;
+                axios.get(route2).then(res => {
+                    const champId: number[] = res.data.slice(0, 5).map((data: any) => parseInt(data['championId']));
+                    const champMastery: number[] = res.data.slice(0, 5).map((data: any) => parseInt(data['championPoints']));
+                    const champLevel: number[] = res.data.slice(0, 5).map((data: any) => parseInt(data['championLevel']));
+
+                    const champName: string[] = champId.map(id => LolApi.getChampName(id));
+                    var champs = champName.map((name, i: number) => {
+                        return {
+                            name: name,
+                            mastery: champMastery[i],
+                            level: champLevel[i]
+                        };
+                    });
+
+                    const route3 = `https://${baseServer}${LolApi.summonerLeagues}${encrySumm}?api_key=${process.env.RIOT_TOKEN}`;
+                    axios.get(route3).then(res => {
+                        const soloqData = res.data.filter((queue: any) => {
+                            return queue['queueType'] == 'RANKED_SOLO_5x5';
+                        })[0];
+
+                        const tier = soloqData['tier'].toLowerCase();
+                        const rank = soloqData['rank'].toUpperCase();
+                        const lp = soloqData['leaguePoints'];
+                        const wins = soloqData['wins'];
+                        const loses = soloqData['losses'];
+                        const winrate = Math.floor((100 * wins) / (wins + loses));
+
+                        embed.setDescription(`He is level ${summLevel}.\n His rank is ${tier} ${rank} ${lp} LP, with ${wins}W ${loses}L - ${(winrate)}%`);
+
+                        champs.forEach((champion) => {
+                            embed.addField(`${champion.name}` , `${(Math.floor(champion.mastery / 1000))} k mastery points, Level ${champion.level}`, false);
+                        });
+    
+                        embed.setTimestamp();
+    
+                        msgObject.channel.send(embed).catch(process.stderr.write);
+                    })
+                }).catch(console.error);
             })
-            .catch(err => {
-                process.stderr.write(err);
-            });
-
-
-        console.log(`${summData.summName} top 5 champions\n`);
-
-        let embed = new Discord.MessageEmbed()
-            .setColor('#0099ff')
-            .setTitle(`${summData.summName} top 5 champions`)
-            .setURL('https://github.com/gramanicu/ShroomBot#readme')
-            .setThumbnail('https://cdn.discordapp.com/app-icons/755011946654335034/5f1aed402fe3b8fb61df8e397510e858.png');
-        
-        // embed.addField(summData.playedChamps[0], '');
-        // embed.addField(summData.playedChamps[1], '');
-        // embed.addField(summData.playedChamps[2], '');
-        // embed.addField(summData.playedChamps[3], '');
-        // embed.addField(summData.playedChamps[4], '');
-        
-        embed.setTimestamp();
-
-        msgObject.channel.send(embed).catch(process.stderr.write);
-    }
+        }
+    };
 }
 
 export default CmdTemplate;
--- src/lolApi.ts
+++ src/lolApi.ts
@@ -1,80 +1,182 @@
-import { LolApi, Constants as LolConstants } from 'twisted'
-import { Regions } from 'twisted/dist/constants';
-import { ChampionsDataDragon, ChampionsDataDragonDetails, ChampionsDataDragonDetailsSolo, SummonerV4DTO } from 'twisted/dist/models-dto';
+export const apiRoute = '.api.riotgames.com';
+export const summonerDataRoute = '/lol/summoner/v4/summoners/by-name/' // + summoner name
+export const champMasteryRoute = '/lol/champion-mastery/v4/champion-masteries/by-summoner/' // + encrypted summoner id
+export const summonerLeagues = '/lol/league/v4/entries/by-summoner/' // + encrypted summoner id
 
-const api = new LolApi(process.env.RIOT_TOKEN);
-let allChampInfo: ChampionsDataDragonDetails[];
-api.DataDragon.getChampion().then(data => {
-    allChampInfo = [];
-    for(let elem of Object.keys(data.data)) {
-        allChampInfo.push(data.data[elem]);
+/**
+ * Returns the actual server name for the specified region
+ * @param region The name of the region 
+ */
+export function checkRegion(region: string): string {
+    switch (region.toLowerCase()) {
+        case 'eune': return 'eun1'
+        case 'euw': return 'euw1'
+        case 'kr': return 'kr'
+        case 'na': return 'na1'
+        default: return '';
     }
-});
-
-export interface SummonerData {
-    summName: string;
-    playedChamps: string[];
 }
 
 /**
- * Converts a region name to a region object
- * @param region A string representing a region (ex. euw, eune, na, kr)
+ * Returns the actual name of a champion
+ * @param id The id of the champion
  */
-async function parseRegion(region: string): Promise<Regions> {
-    switch(region.toLowerCase()) {
-        case 'euw': return Regions.EU_WEST;
-        case 'eune': return Regions.EU_EAST;
-        case 'na': return Regions.AMERICA_NORTH;
-        case 'kr': return Regions.KOREA;
-        default: return Regions.PBE;
+export function getChampName(id: number): string {
+    switch (id) {
+        case 266: return "Aatrox";
+        case 412: return "Thresh";
+        case 23: return "Tryndamere";
+        case 79: return "Gragas";
+        case 69: return "Cassiopeia";
+        case 136: return "Aurelion Sol";
+        case 13: return "Ryze";
+        case 78: return "Poppy";
+        case 14: return "Sion";
+        case 1: return "Annie";
+        case 202: return "Jhin";
+        case 43: return "Karma";
+        case 111: return "Nautilus";
+        case 240: return "Kled";
+        case 99: return "Lux";
+        case 103: return "Ahri";
+        case 2: return "Olaf";
+        case 112: return "Viktor";
+        case 34: return "Anivia";
+        case 27: return "Singed";
+        case 86: return "Garen";
+        case 127: return "Lissandra";
+        case 57: return "Maokai";
+        case 25: return "Morgana";
+        case 28: return "Evelynn";
+        case 105: return "Fizz";
+        case 74: return "Heimerdinger";
+        case 238: return "Zed";
+        case 68: return "Rumble";
+        case 82: return "Mordekaiser";
+        case 37: return "Sona";
+        case 96: return "Kog'Maw";
+        case 55: return "Katarina";
+        case 117: return "Lulu";
+        case 22: return "Ashe";
+        case 30: return "Karthus";
+        case 12: return "Alistar";
+        case 122: return "Darius";
+        case 67: return "Vayne";
+        case 110: return "Varus";
+        case 77: return "Udyr";
+        case 89: return "Leona";
+        case 126: return "Jayce";
+        case 134: return "Syndra";
+        case 80: return "Pantheon";
+        case 92: return "Riven";
+        case 121: return "Kha'Zix";
+        case 42: return "Corki";
+        case 268: return "Azir";
+        case 51: return "Caitlyn";
+        case 76: return "Nidalee";
+        case 85: return "Kennen";
+        case 3: return "Galio";
+        case 45: return "Veigar";
+        case 432: return "Bard";
+        case 150: return "Gnar";
+        case 90: return "Malzahar";
+        case 104: return "Graves";
+        case 254: return "Vi";
+        case 10: return "Kayle";
+        case 39: return "Irelia";
+        case 64: return "Lee Sin";
+        case 420: return "Illaoi";
+        case 60: return "Elise";
+        case 106: return "Volibear";
+        case 20: return "Nunu";
+        case 4: return "Twisted Fate";
+        case 24: return "Jax";
+        case 102: return "Shyvana";
+        case 429: return "Kalista";
+        case 36: return "Dr. Mundo";
+        case 427: return "Ivern";
+        case 131: return "Diana";
+        case 223: return "Tahm Kench";
+        case 63: return "Brand";
+        case 113: return "Sejuani";
+        case 8: return "Vladimir";
+        case 154: return "Zac";
+        case 421: return "Rek'Sai";
+        case 133: return "Quinn";
+        case 84: return "Akali";
+        case 163: return "Taliyah";
+        case 18: return "Tristana";
+        case 120: return "Hecarim";
+        case 15: return "Sivir";
+        case 236: return "Lucian";
+        case 107: return "Rengar";
+        case 19: return "Warwick";
+        case 72: return "Skarner";
+        case 54: return "Malphite";
+        case 157: return "Yasuo";
+        case 101: return "Xerath";
+        case 17: return "Teemo";
+        case 75: return "Nasus";
+        case 58: return "Renekton";
+        case 119: return "Draven";
+        case 35: return "Shaco";
+        case 50: return "Swain";
+        case 91: return "Talon";
+        case 40: return "Janna";
+        case 115: return "Ziggs";
+        case 245: return "Ekko";
+        case 61: return "Orianna";
+        case 114: return "Fiora";
+        case 9: return "Fiddlesticks";
+        case 31: return "Cho'Gath";
+        case 33: return "Rammus";
+        case 7: return "LeBlanc";
+        case 16: return "Soraka";
+        case 26: return "Zilean";
+        case 56: return "Nocturne";
+        case 222: return "Jinx";
+        case 83: return "Yorick";
+        case 6: return "Urgot";
+        case 203: return "Kindred";
+        case 21: return "Miss Fortune";
+        case 62: return "Wukong";
+        case 53: return "Blitzcrank";
+        case 98: return "Shen";
+        case 201: return "Braum";
+        case 5: return "Xin Zhao";
+        case 29: return "Twitch";
+        case 11: return "Master Yi";
+        case 44: return "Taric";
+        case 32: return "Amumu";
+        case 41: return "Gangplank";
+        case 48: return "Trundle";
+        case 38: return "Kassadin";
+        case 161: return "Vel'Koz";
+        case 143: return "Zyra";
+        case 267: return "Nami";
+        case 59: return "Jarvan IV";
+        case 81: return "Ezreal";
+        case 164: return "Camille";
+        case 136: return "Aurelion Sol";
+        case 427: return "Ivern";
+        case 240: return "Kled";
+        case 163: return "Taliyah";
+        case 141: return "Kayn";
+        case 516: return "Ornn";
+        case 142: return "Zoe";
+        case 498: return "Xayah";
+        case 497: return "Rakan";
+        case 555: return "Pyke";
+        case 145: return "Kai'Sa";
+        case 518: return "Neeko";
+        case 235: return "Senna";
+        case 523: return "Aphelios";
+        case 517: return "Sylas";
+        case 350: return "Yuumi";
+        case 246: return "Qiyana";
+        case 875: return "Sett";
+        case 876: return "Lillia";
+        case 777: return "Yone";
+        default: return "Samira";
     }
-};
-
-/**
- * Search data about a champion with the specified id
- * @param champId The id of the champion to be searched
- */
-function getChampionData(champId: number): ChampionsDataDragonDetails {
-    const champ = allChampInfo.filter(champData => champData.key === champId.toString())[0];
-    return champ;
-}
-
-/**
- * Returns data about a particular summoner
- * @param summName The name of the summoner
- * @param regionString The region in which the summoner plays
- */
-export async function summonerByName(summName: string, regionString: string): Promise<SummonerData> {
-    let region;
-    await parseRegion(regionString)
-        .then((data) => {
-            region = data;
-        })
-        .catch(process.stderr.write);
-    let summoner: SummonerV4DTO;
-
-    await api.Summoner.getByName(summName, region)
-        .then((data) => {
-            summoner = data.response;
-        })
-        .catch(() => {
-            process.stderr.write("Couldn't find summoner");
-            // Maybe returns something
-        });
-    
-    await api.Champion.masteryBySummoner(summoner.id, region)
-        .then((data) => {
-            for(let i = 0; i < 1; ++i) {
-                const nm = allChampInfo.filter(champData => {
-                    champData.key === data.response[i].championId.toString();
-                })[0].id;
-                console.log(nm);
-            }
-        })
-        .catch(process.stderr.write)
-
-    var summData: SummonerData;
-    summData.summName = summoner.name;
-
-    return summData;
 }
